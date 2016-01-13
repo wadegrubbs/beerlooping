017dc495d559c06d5b1df38e1912918160e5c7c5
eddie duro and jared beckler <jared.beckler@gmail.com>
2016-01-13 15:58:43 -0800

    update html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
-  var count = 100;
+count = 100;
 
-  for(i = 1; i <= 100; i++) {
-    count = count - 1;
-    document.write(count + " bottles of beer on the wall.")
-  }
+for(i = 1; i <= 100; i++) {
+  count = count - 1;
+  document.write(count + " bottles of beer on the wall.");
+}
 });
