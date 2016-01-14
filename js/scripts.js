c93d660aa926af0ae88b046598a4b503830d8aa7
eddie duro and jared beckler <jared.beckler@gmail.com>
2016-01-13 16:12:22 -0800

    add refresh button and jquery to style

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
 count = 100;
 
-for(i = 1; i <= 100; i++) {
-  count = count - 1;
-  document.write(count + " bottles of beer on the wall.");
-}
+  $(".btn-primary").click(function() {
+    for(i = 1; i <= 100; i++) {
+      count = count - 1;
+      $("div.jumbotron").append("<p>" + count + " bottles of beer on the wall." + "</p>");
+    } if (count <= 0) {
+      $(".btn-primary").hide();
+      $(".btn-success").show();
+    }
+  });
+
+  $(".btn-success").click(function(){
+    window.location.reload();
+  });
 });
